46de5ae687135d45546c2179713af7ddd1d549f5
facu9292 <facundodelgado3875@gmail.com>
2024-07-01 10:51:16 -0300

    ..

---
+++ js/users.js
@@ -0,0 +1,77 @@
+[
+    {
+      "name": "Pedro",
+      "desc": "La comida estuvo muy buena",
+      "stars": "2.5"
+    },
+    {
+      "name": "María",
+      "desc": "El postre fue espectacular",
+      "stars": "4.0"
+    },
+    {
+      "name": "Carlos",
+      "desc": "Las bebidas estaban deliciosas",
+      "stars": "3.5"
+    },
+    {
+      "name": "Ana",
+      "desc": "El servicio fue excelente",
+      "stars": "4.5"
+    },
+    {
+      "name": "Juan",
+      "desc": "Los entrantes fueron decepcionantes",
+      "stars": "2.0"
+    },
+    {
+      "name": "Laura",
+      "desc": "El ambiente del restaurante era acogedor",
+      "stars": "4.0"
+    },
+    {
+      "name": "Diego",
+      "desc": "La presentación de los platos era impresionante",
+      "stars": "4.5"
+    },
+    {
+      "name": "Sofía",
+      "desc": "Los sabores eran auténticos y deliciosos",
+      "stars": "5.0"
+    },
+    {
+      "name": "Javier",
+      "desc": "Los precios eran razonables para la calidad",
+      "stars": "3.5"
+    },
+    {
+      "name": "Elena",
+      "desc": "El menú tenía una gran variedad de opciones",
+      "stars": "4.0"
+    },
+    {
+      "name": "Pablo",
+      "desc": "El postre no estuvo a la altura de las expectativas",
+      "stars": "2.5"
+    },
+    {
+      "name": "Luisa",
+      "desc": "La atención al cliente dejó mucho que desear",
+      "stars": "1.5"
+    },
+    {
+      "name": "Manuel",
+      "desc": "La comida llegó fría y tardaron en servirla",
+      "stars": "2.0"
+    },
+    {
+      "name": "Rosa",
+      "desc": "El restaurante estaba lleno y había mucho ruido",
+      "stars": "3.0"
+    },
+    {
+      "name": "Mario",
+      "desc": "La comida era fresca y bien preparada",
+      "stars": "4.0"
+    }
+  ]
